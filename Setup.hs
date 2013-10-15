a1c615a28eb0b2b0998504209fe34cea890d9686
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2013-10-16 03:15:19 +0800

    cabal build will run happy to generate Parser.hs first

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,10 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.PackageDescription
+import System.Cmd
+
+main = defaultMainWithHooks simpleUserHooks
+  { preBuild = \_ _ -> do
+    putStrLn "happy src/Parser.y"
+    system "happy src/Parser.y"
+    return emptyHookedBuildInfo
+  }
