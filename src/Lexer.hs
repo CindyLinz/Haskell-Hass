63bba6de2613fe2e15a8be0decfaf2a2e47f55e0
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2013-10-21 17:01:04 +0800

    pda Lexer

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE MultiWayIf, OverloadedStrings #-}
 module Lexer
   ( lexer
   , Token (..)
@@ -5,6 +6,7 @@ module Lexer
 
 import qualified Data.Word8 as W
 import Data.Char
+import Data.String
 
 import qualified Data.ByteString as B
 
@@ -19,6 +21,21 @@ data Token
   | T_Space B.ByteString Int
   | T_Comment B.ByteString Comment.Type Comment.Important Int
 
+  | T_StringBegin !W.Word8
+  | T_StringEnd
+  | T_StringBody !B.ByteString
+
+  | T_InterpBegin
+  | T_InterpEnd
+
+  | T_Error B.ByteString
+  deriving Eq
+
+data Mode
+  = CodeMode !Bool -- True for interp, False otherwise
+  | StringMode !W.Word8
+  deriving (Show, Eq)
+
 instance Show Token where
   show (T_Symbol x) = "T_Symbol " ++ [chr $ fromIntegral x]
   show (T_Directive x) = "T_Directive " ++ show x
@@ -29,55 +46,86 @@ instance Show Token where
   show (T_Comment bs t i n) = "T_Comment " ++ show bs ++ " " ++ show t ++ " " ++ show i ++ " (" ++ show n ++ ")"
 
 lexer :: B.ByteString -> [Token]
-lexer bs = case B.uncons bs of
-  Nothing -> []
-  Just (first, bs') ->
-    if isSpace first then
-      let
-        (spaces, bs'') = B.span isSpace bs
-        lineCount = B.count W._lf spaces
-      in T_Space spaces lineCount : lexer bs''
-    else if W._hyphen == first && B.null bs' then
-      T_Symbol W._hyphen : []
-    else if isWord first then
-      takeWord T_Word bs
-    else case B.uncons bs' of
-      Nothing -> T_Symbol first : []
-      Just (second, bs'') -> case isWord second of
-        True
-          | first == W._at -> takeIDWord T_Directive bs'
-          | first == W._percent -> takeIDWord T_Placeholder bs'
-          | first == W._dollar -> takeIDWord T_Variable bs'
-          | otherwise -> fallback
-        False
-          | first == W._slash ->
-            let
-              T_Comment comment _ _ _ = head result
-              importance = if maybe False ((== W._exclam) . fst) (B.uncons comment)
-                then Comment.Important
-                else Comment.NotImportant
-              result
-                | second == W._slash =
-                  let
-                    (c, later) = B.break (== W._lf) bs''
-                    bs''' = B.tail later
-                  in T_Comment c Comment.SingleLine importance 1 : lexer bs'''
-                | second == W._asterisk =
-                  let
-                    (c, later) = B.breakSubstring (B.pack [W._asterisk, W._slash]) bs''
-                    bs''' = B.drop 2 later
-                    lineCount = B.count W._lf c
-                  in T_Comment c Comment.MultiLine importance lineCount : lexer bs'''
-                | otherwise = fallback
-            in result
-          | otherwise -> fallback
-      where
-        fallback = T_Symbol first : lexer bs'
-    where 
-      isIDWord x = isWord x || x == W._hyphen
-      isWord x = W.isAlpha x || W.isDigit x || x == W._underscore || x > 127
-      isSpace x = x <= 127 && W.isSpace x
-      takeWord = takeWordWith isWord
-      takeIDWord = takeWordWith isIDWord
-      takeWordWith pred con bs = con word : lexer bs'
-        where (word, bs') = B.span pred bs
+lexer = go [CodeMode False] where
+  go :: [Mode] -> B.ByteString -> [Token]
+  go allModes@(mode:modes) bs = case B.uncons bs of
+    Nothing -> if
+      | allModes == [CodeMode False] -> []
+      | otherwise -> T_Error (fromString ("lexer mode stack not matched: " ++ show allModes)) : []
+    Just (first, bs') -> case mode of
+      CodeMode interpolating -> if
+        | isSpace first ->
+          let
+            (spaces, bs'') = B.span isSpace bs
+            lineCount = B.count (c '\n') spaces
+          in T_Space spaces lineCount : lexer bs''
+        | c '-' == first && B.null bs' ->
+          T_Symbol (c '-') : []
+        | c '"' == first || c '\'' == first ->
+          T_StringBegin first : go (StringMode first : allModes) bs'
+        | c '}' == first ->
+          ( if interpolating then T_InterpEnd else T_Symbol (c '}') ) : go modes bs'
+        | c '{' == first ->
+          T_Symbol (c '{') : go (CodeMode False : allModes) bs'
+        | isWord first ->
+          takeWord T_Word bs
+        | otherwise -> case B.uncons bs' of
+          Nothing -> T_Symbol first : []
+          Just (second, bs'') -> if
+            | isWord second -> if
+              | c '@' == first -> takeIDWord T_Directive bs'
+              | c '%' == first -> takeIDWord T_Placeholder bs'
+              | c '$' == first -> takeIDWord T_Variable bs'
+              | otherwise -> fallback
+            | otherwise -> if
+              | c '#' == first && c '{' == second ->
+                T_InterpBegin : go (CodeMode True : allModes) bs''
+              | c '/' == first ->
+                let
+                  T_Comment comment _ _ _ = head result
+                  importance = if maybe False ((== c '!') . fst) (B.uncons comment)
+                    then Comment.Important
+                    else Comment.NotImportant
+                  result
+                    | second == c '/' =
+                      let
+                        (content, later) = B.break (== c '\n') bs''
+                        bs''' = B.tail later
+                      in T_Comment content Comment.SingleLine importance 1 : lexer bs'''
+                    | second == c '*' =
+                      let
+                        (content, later) = B.breakSubstring (B.pack (map c "*/")) bs''
+                        bs''' = B.drop 2 later
+                        lineCount = B.count (c '\n') content
+                      in T_Comment content Comment.MultiLine importance lineCount : lexer bs'''
+                    | otherwise = fallback
+                in result
+              | otherwise -> fallback
+          where
+            fallback = T_Symbol first : lexer bs'
+      StringMode delimiter ->
+        let
+          (former, later) = B.span (\ch -> delimiter == ch || c '#' == ch || c '\\' == ch) bs
+          res = if B.null former then resLater else T_StringBody former : resLater
+          resLater = case B.uncons later of
+            Nothing -> T_Error "un-closed string" : []
+            Just (later1, later') -> if
+              | later1 == delimiter -> T_StringEnd : go modes later'
+              | otherwise -> case B.uncons later' of
+                Nothing -> T_StringBody later : go allModes B.empty
+                Just (later2, later'') -> if
+                  | c '#' == later1 && c '{' == later2 -> T_InterpBegin : go (CodeMode True : allModes) later''
+                  | c '\\' == later2 || c '"' == later2 || c '\'' == later2 -> T_StringBody (B.singleton later2) : go allModes later''
+                  | otherwise -> T_StringBody (B.pack [later1, later2]) : go allModes later''
+        in res
+      where 
+        isIDWord x = isWord x || x == c '-'
+        isWord x = W.isAlpha x || W.isDigit x || x == c '_' || x > 127
+        isSpace x = x <= 127 && W.isSpace x
+        takeWord = takeWordWith isWord
+        takeIDWord = takeWordWith isIDWord
+        takeWordWith pred con bs = con word : go allModes bs'
+          where (word, bs') = B.span pred bs
+
+c :: Char -> W.Word8
+c = fromIntegral . ord
